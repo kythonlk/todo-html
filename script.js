9db3b299af78cb82651198736d0e404a54a5c623
Kythonlk <109597262+Kythonlk@users.noreply.github.com>
2023-06-25 14:39:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
-    const taskInput = document.getElementById('task-input');
-    const addTaskButton = document.getElementById('add-task-button');
-    const taskList = document.getElementById('task-list');
+const yearSelect = document.getElementById('year-select');
+const monthBoxes = document.getElementById('month-boxes');
+const dayPicker = document.getElementById('day-picker');
 
-    addTaskButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    const taskText = taskInput.value;
-    if (taskText === '') {
-        return;
-    }
-    const task = document.createElement('div');
-    task.classList.add('task');
-    task.innerHTML = taskText;
-    taskList.appendChild(task);
-    taskInput.value = '';
-    });
+function renderYearSelect() {
+  const currentYear = new Date().getFullYear();
 
-    taskList.addEventListener('click', function(event) {
-    const task = event.target;
-    if (task.classList.contains('task')) {
-        task.remove();
-    }
+  for (let year = currentYear - 10; year <= currentYear + 10; year++) {
+    const option = document.createElement('option');
+    option.value = year;
+    option.textContent = year;
+    yearSelect.appendChild(option);
+  }
+
+  yearSelect.addEventListener('change', function() {
+    const selectedYear = parseInt(yearSelect.value);
+    renderMonthBoxes(selectedYear);
+  });
+
+  yearSelect.value = currentYear;
+}
+
+function renderMonthBoxes(selectedYear) {
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  monthBoxes.innerHTML = '';
+
+  months.forEach((month, index) => {
+    const monthBox = document.createElement('div');
+    monthBox.classList.add('month-box');
+    monthBox.textContent = month;
+    monthBox.addEventListener('click', function() {
+      showDayPicker(selectedYear, index);
     });
+    monthBoxes.appendChild(monthBox);
+  });
+}
+
+function showDayPicker(year, monthIndex) {
+  const daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
+
+  const dayPickerContent = document.createElement('div');
+  dayPickerContent.classList.add('day-picker-content');
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    const dayElement = document.createElement('div');
+    dayElement.classList.add('day');
+    dayElement.textContent = day;
+    dayElement.addEventListener('click', function() {
+      const selectedDate = new Date(year, monthIndex, day).toLocaleDateString();
+      window.location.href = `todo.html?date=${selectedDate}`;
+    });
+    dayPickerContent.appendChild(dayElement);
+  }
+
+  dayPicker.innerHTML = '';
+  dayPicker.appendChild(dayPickerContent);
+}
+
+window.addEventListener('load', function() {
+  const currentYear = new Date().getFullYear();
+  renderYearSelect();
+  renderMonthBoxes(currentYear);
+});
